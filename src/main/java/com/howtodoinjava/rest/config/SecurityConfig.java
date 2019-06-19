98d387b4855f01c98eb3248299001bd34e7e611d
ikarak <ikar777333@gmail.com>
2019-06-19 13:40:20 +0300

    init

---
+++ src/main/java/com/howtodoinjava/rest/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.howtodoinjava.rest.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter
+{
+    @Override
+    protected void configure(HttpSecurity http) throws Exception 
+    {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+            .and()
+                .formLogin()
+                .loginPage("/dist/index.html")
+                .defaultSuccessUrl("/index.html", true)
+                .permitAll()
+            .and()
+                .csrf().disable();
+
+    }
+ 
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) 
+            throws Exception 
+    {
+        auth.inMemoryAuthentication().withUser("admin").password("{noop}admin").roles("USER");
+    }
+
+
+}
