7d496d7a6cacbe67fa6aa6ccc85b9c180037e901
ikarak <ikar777333@gmail.com>
2019-06-21 13:16:27 +0300

    Added prototype of login form

--- src/main/frontend/src/login/LoginForm.js
+++ src/main/frontend/src/login/LoginForm.js
@@ -1,48 +1,56 @@
 import React from "react";
-import FormControl from "@material-ui/core/FormControl";
-import InputLabel from "@material-ui/core/InputLabel";
-import Input from "@material-ui/core/Input";
+import CssBaseline from "@material-ui/core/CssBaseline";
 import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import Container from "@material-ui/core/Container";
+import Paper from "@material-ui/core/Paper";
 
- class LoginForm extends Component {
-     state = {
-       text: ""
-     };
+const style = {
+  Paper: { padding: 20, marginTop: 10, marginBottom: 10 }
+};
 
-     handleChange = e => {
-       this.setState({
-         text: e.target.value
-       });
-     };
+class LoginForm extends React.Component {
 
-     handleSubmit = e => {
-       e.preventDefault();
-       this.props.AddTodo(this.state);
-       this.setState({
-         text: ""
-       });
-     };
+  render() {
+    return (
+      <Container component="main" maxWidth="xs">
+        <Paper style={style.Paper}>
+          <CssBaseline />
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <form name="f" action="login" method="POST">
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <Button type="submit" fullWidth variant="contained" color="primary">
+              Sign In
+            </Button>
+          </form>
+        </Paper>
+      </Container>
+    );
+  }
+}
 
-     render() {
-       return (
-         <div>
-           <form name='f' action="login" method='POST'>
-             <FormControl margin="normal" required fullWidth>
-               <InputLabel htmlFor="text">Username</InputLabel>
-               <Input type='text' name='username' value='' />
-             </FormControl>
-             <FormControl margin="normal" required fullWidth>
-                <InputLabel htmlFor="text">Password</InputLabel>
-                <Input type='password' name='password' value='' />
-             </FormControl>
-             <Button type="submit" fullWidth variant="contained" color="primary">
-               Login
-             </Button>
-           </form>
-         </div>
-       );
-     }
-   }
- }
-
- export default LoginForm;
+export default LoginForm;
