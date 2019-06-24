d30e665652c4fc7d607275713ae5dd2e2068da32
ikar777333 <ikar777333@gmail.com>
2019-06-24 06:57:58 +0300

    init

---
+++ src/main/webapp/WEB-INF/view/react/src/components/home/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline/index";
+import Button from "@material-ui/core/Button/index";
+import Typography from "@material-ui/core/Typography/index";
+import TextField from "@material-ui/core/TextField/index";
+import Container from "@material-ui/core/Container/index";
+import Paper from "@material-ui/core/Paper/index";
